19e1d259f609e87ac1d5d3b85fa7510e82933b41
KEN <ken@KENs-MacBook-Pro.local>
2020-04-26 14:51:04 -0400

    Improve Process bar

--- function.js
+++ function.js
@@ -5,10 +5,30 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
-  if(scrolled === 100){
-    window.alert('Congratulation, you finished all programs!')
+  
+  if (scrolled < 24) {
+    $('a').removeClass("boxy")
+    document.getElementById(`box1`).classList.add("boxy")}
+    else if (scrolled<45) {
+      $('a').removeClass("boxy")
+      document.getElementById(`box2`).classList.add("boxy")
+    }
+    else if (scrolled<64) {
+      $('a').removeClass("boxy")
+      document.getElementById(`box3`).classList.add("boxy")
+    }
+    else if (scrolled<84) {
+      $('a').removeClass("boxy")
+      document.getElementById(`box4`).classList.add("boxy")
+    }
+    else if (scrolled<100) {
+      $('a').removeClass("boxy")
+      document.getElementById(`box5`).classList.add("boxy")
+    }
+    else{
+      window.alert('Congratulation, you finished all programs!')
+    }
   }
-}
 
 $('a').click(function () { 
  
